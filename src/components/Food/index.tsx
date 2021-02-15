81f485ca9fe0211b3822bc5545ff35191192237a
Lucas <lucasfarias.bsb@hotmail.com>
2021-02-15 14:30:37 -0300

    finish

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -4,6 +4,9 @@ import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
+import api from '../../services/api';
+import { boolean } from 'yup';
+
 interface IFoodPlate {
   id: number;
   name: string;
@@ -27,11 +30,20 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+      
+    } catch (error) {
+      console.log(error)
+    }
+    setIsAvailable(!isAvailable);
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
